0d9779d789bf9b27ae3e8d26d88811ec8f6d56bd
23eerick <erickwelberth1@hotmail.com>
2023-03-02 14:20:42 -0300

    feat: connect database

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import dot from 'dotenv'
 import express from 'express'
+import mongoose from 'mongoose'
 
 const app = express()
 app.use(express.json())
@@ -7,11 +8,24 @@ app.use(express.json())
 dot.config().parsed
 
 const Port = process.env.PORT
+const uri = process.env.DATABASE
 
-app.get('/', (req, res) => {
-  res.send('Aplicação iniciada')
-})
+const initdatabase = async () => {
+  await mongoose.connect(uri)
+    .then(() => {
+      console.log('database is connected')
+    })
+    .catch((error) => {
+      console.log(`database is not connected ${error}`)
+    })
+}
 
 app.listen(Port, () => {
   console.log(`API Rest for swell front started at ${Port} 🚀`)
 })
+
+const initApp = async () => {
+  await initdatabase()
+}
+
+initApp()
